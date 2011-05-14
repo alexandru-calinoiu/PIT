0e6f2a2dcf7a5d635ffc9de38149714dd6f03946
calin <calinuswork@gmail.com>
2011-05-14 12:57:41 +0300

    initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PIT::Application.config.secret_token = 'b1bdac2f9cae4598d16250a43135bf52649068f3fc27961cdd6d6dbc869a9d2230b1b6c66b23c70b618647679e1b73939c596e3f0ab1d3aaabffa96bdac96239'
